36747020164808f7f42844fde3937cd02d8c57eb
Vinay Reddy <VinayR@Vinays-MacBook-Pro.local>
2019-09-06 11:34:55 -0400

    Bootcamp1 Completed

--- server.js
+++ server.js
@@ -6,46 +6,34 @@ var http = require('http'),
 /* Global variables */
 var listingData, server;
 
+
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+  if(parsedUrl.pathname == '/listings' && request.method === 'GET') {
+    response.write(listingData);
+    response.end();
+  }
+  else {
+    response.statusCode = 404;
+    response.write('Bad gateway error');
+    response.end();
+  }
 };
 
+server = http.createServer(requestHandler);
+
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
-    HINT: Read up on JSON parsing Node.js
-   */
-
-    //Check for errors
-  
-
-   //Save the sate in the listingData variable already defined
-  
-
-  //Creates the server
-  
-  //Start the server
-
+  if(err){
+    throw err;
+  } 
+  else{
+    listingData = data;
+  }
 
 });
+
+server.listen(port, function() {
+  console.log('Server listening on: http://localhost:' + port);
+}) 
